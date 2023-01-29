b996d91619fdf2f81d6a0694a9900f135f6db01c
djsereno <djsereno91@gmail.com>
2023-01-28 16:15:26 -0800

    Add form validation styling

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
 const pw = document.querySelector("#password");
 const pwConfirm = document.querySelector("#confirm");
+
+email.addEventListener("keyup", setInteracted);
+phone.addEventListener("keyup", setInteracted);
+pw.addEventListener("keyup", setInteracted);
+pwConfirm.addEventListener("keyup", setInteracted);
 
 pw.addEventListener("keyup", verifyPassword);
 pwConfirm.addEventListener("keyup", verifyPassword);
 
 function verifyPassword() {
-  console.log(pw.value, pwConfirm.value, pw.value == pwConfirm.value);
   if (pw.value && pwConfirm.value && pw.value == pwConfirm.value) {
     pw.classList.add("valid");
     pwConfirm.classList.add("valid");
   } else {
     pw.classList.remove("valid");
     pwConfirm.classList.remove("valid");
+  }
+}
+
+function setInteracted(e) {
+  if (e.target.value !== "") e.target.classList.add("interacted");
+  if (e.target.value === "" && !e.target.hasAttribute("required")) {
+    e.target.classList.remove("interacted");
   }
 }
