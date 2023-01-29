04a9e305238931b8373f035fc0c10d107562da5e
djsereno <djsereno91@gmail.com>
2023-01-28 16:48:53 -0800

    Add password verification and confirmation styling

--- script.js
+++ script.js
@@ -6,24 +6,31 @@ const pwConfirm = document.querySelector("#confirm");
 email.addEventListener("keyup", setInteracted);
 phone.addEventListener("keyup", setInteracted);
 pw.addEventListener("keyup", setInteracted);
-pwConfirm.addEventListener("keyup", setInteracted);
-
 pw.addEventListener("keyup", verifyPassword);
-pwConfirm.addEventListener("keyup", verifyPassword);
-
-function verifyPassword() {
-  if (pw.value && pwConfirm.value && pw.value == pwConfirm.value) {
-    pw.classList.add("valid");
-    pwConfirm.classList.add("valid");
-  } else {
-    pw.classList.remove("valid");
-    pwConfirm.classList.remove("valid");
-  }
-}
+pwConfirm.addEventListener("keyup", setInteracted);
+pwConfirm.addEventListener("keyup", confirmPassword);
 
 function setInteracted(e) {
   if (e.target.value !== "") e.target.classList.add("interacted");
   if (e.target.value === "" && !e.target.hasAttribute("required")) {
     e.target.classList.remove("interacted");
   }
+}
+
+function verifyPassword() {
+  password = pw.value;
+  /[a-z]+/.test(password) &&
+  /[A-Z]+/.test(password) &&
+  /[0-9]+/.test(password) &&
+  /[#?!@$%^&*-]+/.test(password) &&
+  password.length >= 8 &&
+  password.length <= 20
+    ? pw.classList.remove("invalid")
+    : pw.classList.add("invalid");
+}
+
+function confirmPassword() {
+  pw.value && pwConfirm.value && pw.value == pwConfirm.value
+    ? pwConfirm.classList.remove("invalid")
+    : pwConfirm.classList.add("invalid");
 }
